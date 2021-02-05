26614d897d0d3295b43c0f576cfbde5f4e534028
dschloe <j2hoon85@gmail.com>
2021-02-05 16:59:56 +0900

    updated

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def getDatabase(client):
     for db in client.list_databases():
         print(db)
 
+def image_upload():
+    return None
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
