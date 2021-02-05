08df90f1e01dc1b7d28022eec4ce0c031c485491
rain0430 <j2hoon85@naver.com>
2021-02-05 15:27:32 +0900

    test

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pymongo import MongoClient
 
+# from _db._pymongo import DataBase
+# from pprint import pprint
+
+def _mongo():
+    client = MongoClient('mongodb://localhost:27017/')
+    print(client.server_info())
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +17,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    _mongo()
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
